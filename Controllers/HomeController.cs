416e4babaa6df0d521878c5b604912a7ec54faf2
SandraSilva12 <124619177+SandraSilva12@users.noreply.github.com>
2023-05-27 14:40:57 +0100

    First Model-Controller-View - Department

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,7 +28,7 @@ namespace SalesWebMVC2.Controllers
             ViewData["Message"] = "Sale Web MVC App from c# Course";
             ViewData["Professor"] = "Sandra Silva";
             
-                return View();
+                return View(); 
         }
 
         public IActionResult Contact()
@@ -45,7 +45,7 @@ namespace SalesWebMVC2.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new SalesWebMVC2.Models.ViewModels.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
