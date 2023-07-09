def14c26738480e2b5e2275385a23cf27c2ffa23
SandraSilva12 <124619177+SandraSilva12@users.noreply.github.com>
2023-07-09 23:34:40 +0100

    Delete seller

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -38,5 +38,29 @@ namespace SalesWebMVC2.Controllers
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindByID(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.remove(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
