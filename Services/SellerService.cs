bc6b4a65ca098e54cdf07afa27ded8de1a1ebbe1
SandraSilva12 <124619177+SandraSilva12@users.noreply.github.com>
2023-07-11 23:41:33 +0100

    Exceptio handling for delete (com erros)

--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -13,56 +13,67 @@ using System.Threading.Tasks;
 
 namespace SalesWebMVC2.Services
 {
-    public class SellerService
-    {
-        private readonly SalesWebMVC2Context _context;
+	public class SellerService
+	{
+		private readonly SalesWebMVC2Context _context;
 
-        public SellerService(SalesWebMVC2Context context)
-        {
-            _context = context;
-        }
+		public SellerService(SalesWebMVC2Context context)
+		{
+			_context = context;
+		}
 
-        public async Task<List<Seller>> FindAllAsync()
-        {
-            return await _context.Seller.ToListAsync();
-        }
-        public async Task InsertAsync(Seller obj)
-        {
-            _context.Add(obj);
-            await _context.SaveChangesAsync();
-        }
+		public async Task<List<Seller>> FindAllAsync()
+		{
+			return await _context.Seller.ToListAsync();
+		}
+		public async Task InsertAsync(Seller obj)
+		{
+			_context.Add(obj);
+			await _context.SaveChangesAsync();
+		}
 
-        public async Task<Seller> FindByIdAsync(int Id)
-        {
-            return await _context.Seller.Include(obj => obj.Department).FirstOrDefaultAsync(obj => obj.Id == id);
-        }
+		public async Task<Seller> FindByIdAsync(int Id)
+		{
+			return await _context.Seller.Include(obj => obj.Department).FirstOrDefaultAsync(obj => obj.Id == id);
+		}
 
-        public async Task RemoveAsync(int id)
-        {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
-        }
+		public async Task RemoveAsync(int id)
+		{
 
-        public async Task UpdateAsync(Seller obj)
-        {
-            bool hasAny = await _context.Seller.AnyAsync(x => x.Id == obj.Id);
+			try
+			{
 
-			if (!hasAny) 
-            {
-                throw new NotFoundException("Id not found");
-            }
-            try
-            {
+				var obj = await _context.Seller.FindAsync(id);
+				_context.Seller.Remove(obj);
+				await _context.SaveChangesAsync();
 
-                _context.Update(obj);
-                await _context.SaveChangesAsync();
-            }
+			}
+			catch (DbUpdateException e)
+			{
+				throw new IntegrityException("Can´t delete seller because he/she has sales");
+			}
+			
+	}
 
-            catch (DbUpdateConcurrencyException e)
-            {
-                throw new DbUpdateConcurrencyException(e.Message);
+		public async Task UpdateAsync(Seller obj)
+		{
+			bool hasAny = await _context.Seller.AnyAsync(x => x.Id == obj.Id);
+
+			if (!hasAny)
+			{
+				throw new NotFoundException("Id not found");
+			}
+			try
+			{
+
+				_context.Update(obj);
+				await _context.SaveChangesAsync();
+			}
+
+			catch (DbUpdateConcurrencyException e)
+			{
+				throw new DbUpdateConcurrencyException(e.Message);
 			}
 		}
-    }
+	}
 }
